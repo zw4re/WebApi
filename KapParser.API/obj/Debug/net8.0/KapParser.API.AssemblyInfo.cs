d7ee89eb03fa21ec12fed4f390a9442876ca8597
Eren  Batu Çapar <erenbatucapar@gmail.com>
2025-07-29 16:18:31 +0300

    s

--- KapParser.API/obj/Debug/net8.0/KapParser.API.AssemblyInfo.cs
+++ KapParser.API/obj/Debug/net8.0/KapParser.API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("KapParser.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8859b16859340671c6a6f0713a17e4fd80e2ddc3")]
 [assembly: System.Reflection.AssemblyProductAttribute("KapParser.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("KapParser.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
